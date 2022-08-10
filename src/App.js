7761d10622faa37b944191cf0de72aa3cbfeb38a
Yusif Shikhseyidov <yusif1580@gmail.com>
2022-08-11 00:40:11 +0400

    PWA Weather App - First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import {fetchWeather} from "./api/fetchWeather";
+import "./App.css";
 
-function App() {
+export default function App() {
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  // Tried to code dynamic image
+  // const bgImage = weather.weather[0].description === "overcast clouds" ? {backgroundImage: url('https://images.pexels.com/photos/9754/mountains-clouds-forest-fog.jpg?auto=compress&cs=tinysrgb&w=1920&h=1920')} : {backgroundImage: url('https://images.pexels.com/photos/1192671/pexels-photo-1192671.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1920')}
+
+
+  const search = async (e) =>{
+    if(e.key === 'Enter'){
+      const data = await fetchWeather(query);
+      setWeather(data);
+      // console.log(data)
+      setQuery("");
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='container'>
+      <input type="text" className='search' placeholder='Type the City Name' value={query} onChange={e => setQuery(e.target.value)} onKeyDown={search} />
+      {weather.main && (<div className='city'>
+        
+        <h1 className='city-name'>
+          <span>{weather.name}</span>
+          <sup>{weather.sys.country}</sup>
+        </h1>
 
-export default App;
+        <div className="city-temp">{Math.round(weather.main.temp)}<sup>&deg;C</sup></div>
+        
+        <div className='info'>
+          <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+          <p>{weather.weather[0].description}</p>
+        </div>
+        
+      </div>)}
+    </div>
+  )
+}
